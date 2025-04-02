409324f4a417596a70e129c6eba5b063dcf9fe55
jsjseop <57559288+jsjseop@users.noreply.github.com>
2025-04-02 18:46:35 +0900

    chore: 명확한 디렉터리만 접근 허용

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
   server: {
     // 특정 도메인만 허용하려면 배열에 추가합니다.
     allowedHosts: ["mercurystudy.store"],
-    // 모든 호스트를 허용하려면 아래와 같이 설정할 수 있습니다.
-    // allowedHosts: 'all',
+    fs: {
+      allow: ['./src', './public'] // 명확한 디렉터리만 허용
+    }
   },
 });
